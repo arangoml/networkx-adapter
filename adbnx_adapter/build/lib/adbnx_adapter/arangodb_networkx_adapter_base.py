221907c451844b3b5a3669f364ad54f16ff19d9e
Rajiv Sambasivan <rajivsam@users.noreply.github.com>
2020-05-11 15:11:44 +0530

    Cert error fix. (#6)
    Co-authored-by: rajiv.sambasivan@gmail.com <rajiv.sambasivan@arangodb.com>

---
+++ adbnx_adapter/build/lib/adbnx_adapter/arangodb_networkx_adapter_base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 11:38:31 2020
+
+@author: Rajiv Sambasivan
+"""
+
+from abc import ABC
+
+class Networkx_Adapter_Base(ABC):
+
+    def create_networkx_graph():
+        pass
