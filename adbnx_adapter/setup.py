1e3baf56bbddc2e77a1b315f4478690f5d523897
rajiv.sambasivan@gmail.com <rajiv.sambasivan@arangodb.com>
2020-05-11 14:44:54 +0530

    Cert error fix.

--- adbnx_adapter/setup.py
+++ adbnx_adapter/setup.py
@@ -9,7 +9,7 @@ HERE = pathlib.Path(__file__).resolve().parents[1]
 # This call to setup() does all the work
 setup(
     name="adbnx_adapter",
-    version="0.0.0.2",
+    version="0.0.0.2.1",
     description="package for creating networkx adapters for arangodb",
     long_description="package for creating networkx adapters for arangodb",
     long_description_content_type="text/markdown",
@@ -21,5 +21,5 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7"],
-    packages=["adbnx_adapter", "adbnx_adapter.dgl", "adbnx_adapter.node2vec"],
+    packages=["adbnx_adapter"],
     include_package_data=True )
