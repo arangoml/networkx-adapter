a206470c2d3e361a38ee6cb82d75fa163363dc60
aMahanna <anthony.mahanna@gmail.com>
2021-10-15 13:18:37 -0400

    Update main.py

--- adbnx_adapter/main.py
+++ adbnx_adapter/main.py
@@ -2,6 +2,8 @@ import json
 import networkx as nx
 from adbnx_adapter.arangoDB_networkx_adapter import ArangoDB_Networkx_Adapter
 from adbnx_adapter.imdb_arangoDB_networkx_adapter import IMDBArangoDB_Networkx_Adapter
+from adbnx_adapter.dgl_arangoDB_networkx_adapter import DGLArangoDB_Networkx_Adapter
+import matplotlib.pyplot as plt
 
 # Specify the connection to the ArangoDB Database
 con = {
@@ -16,6 +18,7 @@ con = {
 # Create Adapter instance
 ma = ArangoDB_Networkx_Adapter(conn=con)
 # ma = IMDBArangoDB_Networkx_Adapter(conn=con)
+itsmg = DGLArangoDB_Networkx_Adapter(con)
 
 fraud_detection_attributes = {
     "vertexCollections": {
@@ -37,9 +40,47 @@ imdb_attributes = {
     "edgeCollections": {"Ratings": {"_from", "_to", "ratings"}},
 }
 
-g = ma.create_networkx_graph(
-    graph_name="fraud-detection", graph_attributes=fraud_detection_attributes
-)
+vcols = {
+    "incident": {
+        "D_sys_mod_count",
+        "D_sys_mod_count",
+        "D_reopen_count",
+        "urgency",
+        "incident_state",
+        "u_symptom",
+        "impact",
+        "contact_type",
+        "u_priority_confirmation",
+        "cmdb_ci",
+        "rfc",
+        "problem_id",
+        "caused_by",
+        "location",
+        "knowledge",
+        "resolved_by",
+        "subcategory",
+        "active",
+        "category",
+        "priority",
+        "reassigned",
+        "node_id",
+    },
+    "support_org": {"assigned_to", "assignment_group", "node_id"},
+    "customer": {"opened_by", "node_id"},
+    "vendor": {"vendor", "node_id"},
+}
+ecols = {
+    "incident_support_org": {"_from", "_to"},
+    "incident_customer": {"_from", "_to"},
+    "incident_vendor": {"_from", "_to"},
+}
+
+itsm_attributes = {"vertexCollections": vcols, "edgeCollections": ecols}
+
+# ----------------------------------- Fraud Detection -----------------------------------
+# g = ma.create_networkx_graph(
+#     graph_name="fraud-detection", graph_attributes=fraud_detection_attributes
+# )
 
 # g = ma.create_networkx_graph_from_graph(
 #     graph_name="fraud-detection", ttl=11
@@ -51,7 +92,13 @@ g = ma.create_networkx_graph(
 #     edge_collections={"accountHolder", "Relationship", "transaction"},
 # )
 
+# ----------------------------------- IMDB -----------------------------------
 # g = ma.create_networkx_graph(graph_name="IMDBGraph", graph_attributes=imdb_attributes)
+
+# ----------------------------------- DGL -----------------------------------
+g, labels = itsmg.create_dgl_graph(
+    graph_name="ITSMGraph", graph_attributes=itsm_attributes
+)
 
 # nx.draw(g, with_labels=True)
 
