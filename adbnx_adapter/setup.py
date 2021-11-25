89a7a174497ad99cdb62d25309580990d64c92b5
aMahanna <anthony.mahanna@gmail.com>
2021-11-24 23:03:09 -0500

    fix: copy README to package directory in release environment

--- adbnx_adapter/setup.py
+++ adbnx_adapter/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("VERSION") as f:
     version = f.read().strip()
 
-with open("../README.md", "r") as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
