06d0544ea4ab3b57078ca5a59b6aa74b76a6c98c
aMahanna <anthony.mahanna@gmail.com>
2021-12-23 11:47:20 -0500

    new: coveralls integration

--- adbnx_adapter/setup.py
+++ adbnx_adapter/setup.py
@@ -33,6 +33,7 @@ setup(
             "mypy>=0.790",
             "pytest>=6.0.0",
             "pytest-cov>=2.0.0",
+            "coveralls>=3.3.1",
             "types-setuptools",
             "types-requests",
         ],
