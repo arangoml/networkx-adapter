75ac2b3a49666646890b2a8a4035bb477b5aa115
aMahanna <anthony.mahanna@gmail.com>
2022-07-25 19:43:10 -0400

    cleanup: `rich` progress

--- adbnx_adapter/adapter.py
+++ adbnx_adapter/adapter.py
@@ -429,7 +429,7 @@ class ADBNX_Adapter(Abstract_ADBNX_Adapter):
                     RETURN doc
             """
 
-        with progress(f"Fetching '{col}' documents") as p:
+        with progress(f"Export: {col}") as p:
             p.add_task("__fetch_adb_docs")
 
             return self.__db.aql.execute(
@@ -448,7 +448,7 @@ class ADBNX_Adapter(Abstract_ADBNX_Adapter):
             https://docs.python-arango.com/en/main/specs.html#arango.collection.Collection.import_bulk
         """
         for col, doc_list in adb_documents.items():
-            with progress(f"Inserting '{col}' documents ({len(doc_list)})") as p:
+            with progress(f"Import: {col} ({len(doc_list)})") as p:
                 p.add_task("__insert_adb_docs")
 
                 result = self.__db.collection(col).import_bulk(doc_list, **kwargs)
